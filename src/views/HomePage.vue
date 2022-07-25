6c70ab9d81326fee002daf93144fbb707c6cc377
nap-owen <nap.owenmontemor@gmail.com>
2022-07-25 18:29:44 +0800

    added my project

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,26 +1,3 @@
-<template>
-  <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
-    </ion-content>
-  </ion-page>
-</template>
-
 <script lang="ts">
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
 import { defineComponent } from 'vue';
@@ -37,15 +14,55 @@ export default defineComponent({
 });
 </script>
 
+<template>
+  <ion-page>
+    <!-- <ion-header :translucent="true">
+      <ion-toolbar class="header">
+        <ion-title>Translucent Blank</ion-title>
+      </ion-toolbar>
+    </ion-header> -->
+    
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Fullscreen Blank</ion-title>
+        </ion-toolbar>
+      </ion-header>
+    
+      <div id="container">
+        <img id="bg-img" src="assets/svg/login_bg.svg" alt="">
+        <!-- <strong>Ready to create an app?</strong>
+        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p> -->
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
 <style scoped>
+
+ion-content {
+  --ion-background-color: white !important;
+  position: relative;
+}
+
+.header {
+  --background: white !important;
+}
+
 #container {
   text-align: center;
-  
+  height: 100vh;
   position: absolute;
   left: 0;
   right: 0;
-  top: 50%;
-  transform: translateY(-50%);
+  /* border: 1px solid red; */
+}
+
+#bg-img {
+  position: absolute;
+  top:0;
+  left: 0;
+  height: 100%;
 }
 
 #container strong {
